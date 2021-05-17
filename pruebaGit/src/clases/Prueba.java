c04318bf90f8dcb60216d836d37a627a828537c3
byrocket09 <83572660+byrocket09@users.noreply.github.com>
2021-05-17 09:27:42 +0200

    Update Prueba.java

--- pruebaGit/src/clases/Prueba.java
+++ pruebaGit/src/clases/Prueba.java
@@ -5,7 +5,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("modificado");
+		System.out.println("modificado2");
 		System.out.println("modifGithub");
 		
 	}
